9a400675a8528ab07af583943aea7ebb480cc928
jplwopdev <joaopedrolimanogueira369@gmail.com>
2025-04-13 12:57:03 -0300

    tentativa n°1 de commit pelo vscode

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'jaka':['Roboto', 'sans-serif'],
+    },
+    },
+  },
+})
