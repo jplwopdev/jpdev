ab789a053640f661ab53c1b0267513ca618efc26
jplwopdev <joaopedrolimanogueira369@gmail.com>
2025-04-26 21:51:48 -0300

    Aprimorar a interação do SVG: remover eventos de hover e simplificar o código

--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
 import './style.css';
-
-const div = document.getElementById('meuSvg');
-const svg = document.getElementById('meuSvg');
-const grupo = svg.querySelector('g');
-
-svg.addEventListener('mouseenter', () => {
-  grupo.setAttribute('fill', '#ffffff'); // Branco no hover
-});
-
-svg.addEventListener('mouseleave', () => {
-  grupo.setAttribute('fill', '#c10007'); // Vermelho ao sair
-});
